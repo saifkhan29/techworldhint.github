7dd5675216bfcade42f1202ce41b2e9adcf8d328
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2021-06-15 19:09:49 +0530

    Add files via upload

---
+++ Asus Laptops/app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+const links = document.querySelectorAll('.nav-links li')
+
+
+
+
+
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open')
+    
+    links.forEach(link  => {
+      if(navLinks.classList.contains('open')) {
+        link.classList.add('fade');
+      } else {
+        link.classList.remove('fade');
+      }
+    });
+})
